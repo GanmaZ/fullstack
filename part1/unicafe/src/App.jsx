e2e3ebee73fcd764a37d3fc401c4dad482fc1bd7
GanmaZ <jimegch@hotmail.com>
2024-04-24 11:43:49 -0500

    1.8: unicafe, step 3

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,22 +2,21 @@ import { useState } from 'react'
 
 const Display = ({counter, text}) => <div>{text} {counter}</div>
 const Button = ({ onSmash, text }) => <button onClick={onSmash}>{text}</button>
-const MoreStatistics = ({option, good, neutral, bad}) => {
-  switch (option) {
+const Statistics = (props) => {
+  switch (props.option) {
     case "all":
-      return(<div>{option} {good+neutral+bad}</div>)
+      return(<div>{props.option} {props.good+props.neutral+props.bad}</div>)
       break;
     case "average":
-      return(<div>{option} {((good*1)+(neutral*0)+(bad*-1))/(good+neutral+bad)}</div>)
+      return(<div>{props.option} {((props.good*1)+(props.neutral*0)+(props.bad*-1))/(props.good+props.neutral+props.bad)}</div>)
       break;
     case "positive":
-      return(<div>{option} {(100 * good) / (good+neutral+bad)} %</div>)
+      return(<div>{props.option} {(100 * props.good) / (props.good+props.neutral+props.bad)} %</div>)
       break;
     default:
       break;
   }
 }
-
 
 
 const App = () => {
@@ -26,30 +25,19 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const increaseGood = () => {
-    setGood(good + 1)
-  }
-  const increaseNeutral = () => {
-    setNeutral(neutral + 1)
-  }
-  const increaseBad = () => {
-    setBad(bad + 1)
-  }
-
-
   return (
     <div>
       <h1>give feedback</h1>
-      <Button onSmash={increaseGood} text="good" />
-      <Button onSmash={increaseNeutral} text="neutral" />
-      <Button onSmash={increaseBad} text="bad" />
+      <Button onSmash={() => setGood(good + 1)} text="good" />
+      <Button onSmash={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button onSmash={() => setBad(bad + 1)} text="bad" />
       <h1>statistics</h1>
       <Display counter={good} text="good"/>
       <Display counter={neutral} text="neutral"/>
       <Display counter={bad} text="bad"/>
-      <MoreStatistics option={"all"} good={good} neutral={neutral} bad={bad}/>
-      <MoreStatistics option={"average"} good={good} neutral={neutral} bad={bad}/>
-      <MoreStatistics option={"positive"} good={good} neutral={neutral} bad={bad}/>
+      <Statistics option={"all"} good={good} neutral={neutral} bad={bad}/>
+      <Statistics option={"average"} good={good} neutral={neutral} bad={bad}/>
+      <Statistics option={"positive"} good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
