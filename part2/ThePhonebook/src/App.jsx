73a66d137808cfac199b1b627d1f9843fb8a0d62
GanmaZ <jimegch@hotmail.com>
2024-05-06 10:44:08 -0500

    2.10 phonebook step5

--- part2/ThePhonebook/src/App.jsx
+++ part2/ThePhonebook/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
-import Phone from './components/Phone'
+import Personform from './components/PersonForm'
+import Persons from './components/Persons'
+import Filter from './components/Filter'
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -13,7 +15,7 @@ const App = () => {
   const [showFilter, setShowFilter] = useState([])
 
   const addPerson = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     const personObject = {
     name: newName,
     number: newNumber
@@ -30,7 +32,7 @@ const App = () => {
     }
   }
   const handleNumberChange = (event) => {
-      setNewNumber(event.target.value)
+    setNewNumber(event.target.value)
   }
   const handleFilterChange = (event) => {
     setShowFilter(persons.filter(x => x.name.toLowerCase().includes(event.target.value.toLowerCase())))
@@ -39,34 +41,11 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-      filter show with: 
-          <input 
-            onChange={handleFilterChange}          
-          />
-      </div>
+      <Filter filterChangeHandler={handleFilterChange}/>
       <h2>add a new</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: 
-          <input 
-            value={newName} 
-            onChange={handleNameChange}          
-          />
-        </div>
-        <div>
-          number: 
-            <input 
-              value={newNumber} 
-              onChange={handleNumberChange}          
-            />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <Personform addPerson={addPerson} nameChangeHandler={handleNameChange} numberChangeHandler={handleNumberChange} newName={newName}  newNumber={newNumber}/>
       <h2>Numbers</h2>
-      {showFilter.length === 0 ? setShowFilter(persons) : showFilter.map(person => <Phone key={person.name} phone={person}/>)}
+      <Persons showFilter={showFilter} persons={persons}/>
     </div>
   )
 }
