a5fcc4afd5cad54aa45353f470c2b53983d7526d
GanmaZ <jimegch@hotmail.com>
2024-05-07 15:40:22 -0500

    2.18 DataCountries step 1

--- part2/DataforCountries/src/App.jsx
+++ part2/DataforCountries/src/App.jsx
@@ -1,19 +1,32 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Find from './components/Find'
+import Countries from './services/Countries'
+import CountriesBody from './components/CountriesBody'
 
 
 function App() {
 
+  const [countries, setCountries] = useState(null)
+  const [countriesFind, setCountriesFind] = useState("")
   const [newFinder,setNewFinder] = useState(null)
   
+
+  useEffect(() => {
+      Countries
+      .getCountries()
+      .then(initialCountries => setCountries(initialCountries))
+  }, [])
+
+  
   const handleFinderChange = (event) => {
-    setNewFinder(event.target.value)
+    setCountriesFind(countries.map(country => country).filter(country => country.name.common.toLowerCase().includes(event.target.value.toLowerCase())))
   }
 
 
   return (
     <>
        <p>find countries <Find finderChangeHandler={handleFinderChange}/></p>
+       <CountriesBody  countriesFind={countriesFind}/> 
     </>
   )
 }
