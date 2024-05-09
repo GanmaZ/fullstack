db5b8d47dce7153b17b0d974ba987e2467f94c97
GanmaZ <jimegch@hotmail.com>
2024-05-08 19:26:04 -0500

    render updt

--- part3/index.js
+++ part3/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
+app.use(cors())
 
 
 let phonebook = [
@@ -97,7 +99,7 @@ morgan.token('data', function (req, res) { return JSON.stringify(req.body) })
     response.json(phone)
   })
   
-  const PORT = 3001
+  const PORT = process.env.PORT || 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
